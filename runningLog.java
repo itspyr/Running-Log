5b8258d1bb16a76a07f79f04bc0e6945370a0b80
itspyr <itspyr@outlook.com>
2025-05-28 11:20:35 -0400

    Update runningLog.java

--- runningLog.java
+++ runningLog.java
@@ -7,15 +7,17 @@ public class runningLog {
         double time;
         System.out.println("How many runs did you go on?");
         Scanner input = new Scanner(System.in);
-        numberOfRun = input.nextInt();
+        numberOfRun = input.nextInt(); //Total number of runs completed (User entry)
 
         for (int i = 0; i < numberOfRun; ++i) {
             System.out.println("How long was the distance in miles?");
-            double distanceInMiles = input.nextDouble();
-            System.out.println("What was the time in minutes?");
-            double timeInMinutes = input.nextDouble();
+            double distanceInMiles = input.nextDouble(); //Distance ran (User entry)
 
-            double averagePace = timeInMinutes / distanceInMiles;
+            System.out.println("What was the time in minutes?");
+            double timeInMinutes = input.nextDouble(); //Time spent during run (User entry)
+
+            double averagePace = timeInMinutes / distanceInMiles; //Average calculation per run (Automatic)
+
             System.out.println("The average pace is " + String.format("%.1f", averagePace)); //String.format("%.1f" is for decimal rounding!
 
         }
